446a3b22e65bac14df6632ba430bd1ee1bff2736
Htet Min Khant <hmk469@gmail.com>
2023-10-19 22:28:00 +0630

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+    list.forEach(item => {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseenter", activeLink));
+
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector('.main');
+
+toggle.addEventListener("click", function () {
+    navigation.classList.toggle('active');
+    main.classList.toggle("active");
+});
